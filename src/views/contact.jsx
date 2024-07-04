a3503b98eff3666e4024bf8d352e07e2b38bbdd9
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2024-07-04 19:35:06 -0400

    ✨ A contact card is added

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -1,23 +1,160 @@
 import {
-    Flex,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  import ServiceCard from '../components/serviceCard'; 
-  import DividerBar from '../components/dividerBar';
-  import services from '../assets/ej_services';
-  import Pricing from '../components/pricing';
-    
-  function Services() {
-      return (
-          <Flex
-              minH={'50vh'}
-              align={'center'}
-              justify={'center'}
-              direction="column"
-              bg={useColorModeValue('#0000', '#0000')}>
-          </Flex>
-      );
-  }
-  
-  export default Services;
-  
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from '@chakra-ui/react'
+import {
+  MdPhone,
+  MdEmail,
+  MdLocationOn,
+  MdFacebook,
+  MdOutlineEmail,
+} from 'react-icons/md'
+import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs'
+
+export default function Contact() {
+  return (
+    <Container bg="white" maxW="full" mt={0} centerContent overflow="hidden">
+      <Flex>
+        <Box
+          bg="#2D284A"
+          color="#BEBDC0"
+          borderRadius="lg"
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}>
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Heading>Contacto</Heading>
+                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="#BEBDC0">
+                    Llena el formulario con tus datos
+                  </Text>
+                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                    <VStack pl={0} spacing={3} alignItems="flex-start">
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color="#BEBDC0"
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
+                        +56 9 1234 5678
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color="#BEBDC0"
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
+                        contacto@mail.com
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color="#BEBDC0"
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
+                        Peñalolen, Region Metropolitana
+                      </Button>
+                    </VStack>
+                  </Box>
+                  <HStack
+                    mt={{ lg: 10, md: 10 }}
+                    spacing={5}
+                    px={5}
+                    alignItems="flex-start">
+                    <IconButton
+                      aria-label="facebook"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: '#0D74FF' }}
+                      icon={<MdFacebook size="28px" />}
+                    />
+                    <IconButton
+                      aria-label="github"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: '#0D74FF' }}
+                      icon={<BsGithub size="28px" />}
+                    />
+                    <IconButton
+                      aria-label="discord"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: '#0D74FF' }}
+                      icon={<BsDiscord size="28px" />}
+                    />
+                  </HStack>
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box bg="#585174" borderRadius="lg">
+                  <Box m={8} color="#0B0E3F">
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>Tu nombre</FormLabel>
+                        <InputGroup borderColor="black">
+                          <InputLeftElement pointerEvents="none">
+                            <BsPerson color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Correo electronico</FormLabel>
+                        <InputGroup borderColor="black">
+                          <InputLeftElement pointerEvents="none">
+                            <MdOutlineEmail color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Mensaje</FormLabel>
+                        <Textarea
+                          borderColor="black"
+                          _hover={{
+                            borderRadius: 'black',
+                          }}
+                          placeholder="message"
+                        />
+                      </FormControl>
+                      <FormControl id="name" float="right">
+                        <Button variant="solid" bg="#2D284A" color="#BEBDC0" _hover={{}}>
+                          Enviar mensaje
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
+  )
+}
