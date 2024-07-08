49439ebe7b38b373142bf15ef856557c00abed34
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2024-07-07 23:07:26 -0400

    🚀 Deploy Stuff

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/WebSite_Bikes/',
+  base: 'https://zurickata.github.io/WebSite_Bikes/',
   plugins: [react()]
 })
