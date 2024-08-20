a06ec20c3bfba186bc2f955d8557d973c5bad9c8
Zurickata <rodrigoramirezcatrileo@gmail.com>
2024-08-19 21:51:19 -0400

    ContactForm Ready

--- apps/server/src/email/email.service.ts
+++ apps/server/src/email/email.service.ts
@@ -29,7 +29,7 @@ export class EmailService {
       `,
       attachments: imagenes?.map((file) => ({
         filename: file.originalname,
-        path: file.buffer,
+        content: file.buffer,
       })),
     };
 
